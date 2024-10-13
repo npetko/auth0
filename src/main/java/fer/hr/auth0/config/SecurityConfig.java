29267332b208c8994bd1d05d5b5abdde59213608
npetko <npnikolapetkovic@gmail.com>
2024-10-13 18:32:13 +0200

    Add secutiry config

---
+++ src/main/java/fer/hr/auth0/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package fer.hr.auth0.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/tickets/generate").authenticated()
+                        .requestMatchers("/api/tickets/**").permitAll()
+                )
+                .oauth2Login((oauth2Login) -> oauth2Login
+                        .userInfoEndpoint((userInfo) -> userInfo
+                                .userAuthoritiesMapper(grantedAuthoritiesMapper())
+                        )
+                );
+        return http.build();
+    }
+
+    private GrantedAuthoritiesMapper grantedAuthoritiesMapper() {
+        return (authorities) -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+            authorities.forEach((authority) -> {
+                GrantedAuthority mappedAuthority;
+
+                if (authority instanceof OidcUserAuthority) {
+                    OidcUserAuthority userAuthority = (OidcUserAuthority) authority;
+                    mappedAuthority = new OidcUserAuthority(
+                            "ROLE_USER", userAuthority.getIdToken(), userAuthority.getUserInfo());
+                } else if (authority instanceof OAuth2UserAuthority) {
+                    OAuth2UserAuthority userAuthority = (OAuth2UserAuthority) authority;
+                    mappedAuthority = new OAuth2UserAuthority(
+                            "ROLE_USER", userAuthority.getAttributes());
+                } else {
+                    mappedAuthority = authority;
+                }
+
+                mappedAuthorities.add(mappedAuthority);
+            });
+
+            return mappedAuthorities;
+        };
+    }
+
+}
