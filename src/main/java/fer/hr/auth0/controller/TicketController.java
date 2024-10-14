4f8ee10ca6f998ec08abde4fa8dfeab9ce7dc9b4
npetko <npnikolapetkovic@gmail.com>
2024-10-14 06:09:27 +0200

    Add ticketing functions to contoller

--- src/main/java/fer/hr/auth0/controller/TicketController.java
+++ src/main/java/fer/hr/auth0/controller/TicketController.java
@@ -3,18 +3,24 @@ package fer.hr.auth0.controller;
 import fer.hr.auth0.dto.TicketRequest;
 import fer.hr.auth0.model.Ticket;
 import fer.hr.auth0.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/tickets")
+@CrossOrigin(value = "*", allowedHeaders = "*")
 public class TicketController {
 
+    @Autowired
     private TicketService ticketService;
 
+    @PostMapping("/generate")
     public ResponseEntity<?> generateTicket(@RequestBody TicketRequest ticketRequest) {
         try {
             Ticket ticket = ticketService.createTicket(
@@ -22,10 +28,23 @@ public class TicketController {
                     ticketRequest.getFirstName(),
                     ticketRequest.getLastName()
             );
-            return new ResponseEntity<>(HttpStatusCode.valueOf(200));
+            return ResponseEntity.ok(ticket);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @GetMapping("/count")
+    @CrossOrigin(value = "*", allowedHeaders = "*")
+    public ResponseEntity<Long> getTotalTickets() {
+        Long count = ticketService.getTotalTicketsCount();
+        return ResponseEntity.ok(count);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getTicketDetails(@PathVariable UUID id) {
+        Optional<Ticket> ticket = ticketService.getTicketById(id);
+        return ticket.isPresent() ? ResponseEntity.ok(ticket.get()) : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Ticket not found");
     }
 
 }
--- src/main/java/fer/hr/auth0/service/TicketService.java
+++ src/main/java/fer/hr/auth0/service/TicketService.java
@@ -2,13 +2,17 @@ package fer.hr.auth0.service;
 
 import fer.hr.auth0.model.Ticket;
 import fer.hr.auth0.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class TicketService {
 
+    @Autowired
     private TicketRepository ticketRepository;
 
     public List<Ticket> getTicketsByVatin(String vatin) {
@@ -20,6 +24,14 @@ public class TicketService {
             throw new Exception("Maximum number of tickets reached for OIB: " + vatin);
         }
         return ticketRepository.save(new Ticket(vatin, firstName, lastName));
+    }
+
+    public Long getTotalTicketsCount() {
+        return ticketRepository.count();
+    }
+
+    public Optional<Ticket> getTicketById(UUID id) {
+        return ticketRepository.findById(id);
     }
 
 }
